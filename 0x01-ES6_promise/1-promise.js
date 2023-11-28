f9eb5241f0bca4e048d69655cace0b5a66dc5ff7
Terry Wambui <shikuwambui@gmail.com>
2023-11-28 22:44:10 +0300

    return a promise with a boolean parameter

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,12 +1,6 @@
-function getFullResponseFromAPI(success) {
+export default function getFullResponseFromAPI(success) {
   return new Promise((resolve, reject) => {
-    if (success) {
-      resolve({ status: 200, body: 'Succcess' });
-    } else {
-      reject(Error('The fake API is not working currently'));
-    }
+    if (success) resolve({ status: 200, body: 'Success' });
+    else reject(Error('The fake API is not working currently'));
   });
 }
-
-// Export the function for external use
-export default getFullResponseFromAPI;
