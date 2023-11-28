769177273e55411b9fca58c64bc78ed7480b23ce
Terry Wambui <shikuwambui@gmail.com>
2023-11-29 00:40:25 +0300

    All your promises

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,4 +1,4 @@
-import { uploadPhoto, createUser } from '../utils';
+import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
