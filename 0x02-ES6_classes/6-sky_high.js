5a911802dbcd87fd0b37126be12c8c627e45be11
Terry Wambui <shikuwambui@gmail.com>
2023-11-30 18:04:12 +0300

    OverRide

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,18 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this._floors = floors;
+  }
+
+  // Getter for floors
+  get floors() {
+    return this._floors;
+  }
+
+  // override method
+  evacuationWarningMessage() {
+    return (`Evacuate slowly the ${this.floors} floors`);
+  }
+}
