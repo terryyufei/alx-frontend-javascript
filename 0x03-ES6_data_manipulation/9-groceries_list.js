a573ac36497fbd28c85b1246d7cbab9279922fb1
Terry Wambui <shikuwambui@gmail.com>
2023-12-05 00:47:57 +0300

    Map Data structure

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,10 @@
+export default function groceriesList() {
+  const shoppingList = new Map([
+    ['Apples', 10],
+    ['Tomatoes', 10],
+    ['Pasts', 1],
+    ['Rice', 1],
+    ['Banana', 5],
+  ]);
+  return shoppingList;
+}
